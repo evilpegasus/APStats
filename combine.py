345410d8d8dc23d73a35a729741411f91f4bc1a9
Ming Fong <mingfong07@hotmail.com>
2020-01-16 00:35:45 -0800

    linear regression

--- combine.py
+++ combine.py
@@ -2,6 +2,8 @@ import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import numpy as np
 import average_salary
 import sat
 
@@ -14,14 +16,31 @@ print(salary_df)
 #combine sat and salary dataframes into a combined dataframe
 #take only the sat and average salary columns
 combined_df = pd.concat([sat_df, salary_df], axis=1, sort=True)[['Total Score Mean', 'Average Salary']]
+#remove rows with empty (NaN) values
+combined_df = combined_df.dropna(axis = 0) #uncomment to fix pairing issues when district names don't match up
+
 print(combined_df)
 print("Combined dataframe has " + str(combined_df.shape[0]) + " data points")
 
 #copy combined dataframe to Excel
-#combined_df.to_excel("combined.xlsx")
+combined_df.to_excel("combined.xlsx")
+
+#linear regression
+X = combined_df['Average Salary'].values.reshape(-1, 1)
+Y = combined_df['Total Score Mean'].values.reshape(-1, 1)
+linear_regressor = LinearRegression()
+linear_regressor.fit(X, Y)
+Y_pred = linear_regressor.predict(X)
 
 #plot data
 ax = combined_df.plot.scatter(x='Average Salary', y='Total Score Mean')
 ax.set_xlim(xmin = 0)
 ax.set_ylim(bottom = 0)
+plt.title('Mean SAT Scores vs. Mean Salaries')
+plt.xlabel('Mean SAT Score')
+plt.ylabel('Average Secondary Teacher Salary')
+
+#plot regression line
+plt.plot(X, Y_pred, color='red')
+
 plt.show()
