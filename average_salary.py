67daf75dd83bc6fa5f182f3246791bcfeb059c9d
Ming Fong <mingfong07@hotmail.com>
2020-01-15 22:07:02 -0800

    clean up

--- average_salary.py
+++ average_salary.py
@@ -7,24 +7,30 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-df = pd.read_excel('WA_Secondary_Teachers_2018-2019.xlsx', usecols= [0, 2, 6])
+
+
+
+
+#Teacher salary data
+
+df = pd.read_excel('WA_Secondary_Teachers_2018-2019.xlsx', index_col=0, usecols= [0, 2, 6])
 df['Average Salary'] = df.groupby('School District')['Total Salary'].transform('mean')
 
-#print
-#print("Column headings:")
-#print(df.columns)
 print(df)
 
 districts = df.groupby('School District', as_index=True).nunique()
 districts['Average Salary'] = df.groupby('School District')['Total Salary'].mean()
-#districts['Average Salary'] = df.groupby('School District')['Average Salary']
 
 print(districts)
-#districts.to_excel('districts.xlsx')
 
 #Number of rows (unique districts)
 print("There are " + str(districts.shape[0]) + " districts")
 print("Average of average secondary teacher salary by district is " + str(districts['Average Salary'].mean()))
 
+#Copy averages to Excel
+#districts.to_excel("average_salary.xlsx")
 
-districts.to_excel("average_salary.xlsx")
+
+
+
+
